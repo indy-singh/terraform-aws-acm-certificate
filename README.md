72152a31241705169e7e1dc44e9a537a9f645728
indy-singh <1906778+indy-singh@users.noreply.github.com>
2024-04-19 00:19:30 +0100

    Make it clear this is pluggable

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ module "my_acm_certificate" {
   }
 
   providers = {
-    aws.virginia = aws.virginia
+    aws.region = aws.virginia
   }
 }
 ```
--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_acm_certificate" "main" {
-  provider                  = aws.virginia
+  provider                  = aws.region
   domain_name               = keys(var.zone_ids)[0]
   subject_alternative_names = slice(keys(var.zone_ids), 1, length(var.zone_ids))
   validation_method         = "DNS"
 }
 
 resource "aws_route53_record" "validation" {
-  provider = aws.virginia
+  provider = aws.region
   for_each = {
     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -24,7 +24,7 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "main" {
-  provider                = aws.virginia
+  provider                = aws.region
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for rec in aws_route53_record.validation : rec.fqdn]
 }
